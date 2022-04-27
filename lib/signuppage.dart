6788b82735fb9dc25bb7523108922f72ed025570
Gentle_wane <adebayooluwatibi2018@gmail.com>
2022-04-27 15:54:46 +0100

    i

--- lib/signuppage.dart
+++ lib/signuppage.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 class Signuppage extends StatefulWidget {
   const Signuppage({Key? key}) : super(key: key);
 
@@ -11,8 +13,9 @@ class _SignuppageState extends State<Signuppage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Container(
-        padding: const EdgeInsets.all(20.00),
+        padding: const EdgeInsets.all(25.00),
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
@@ -23,6 +26,7 @@ class _SignuppageState extends State<Signuppage> {
         constraints: const BoxConstraints.expand(),
         child: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Align(
                 alignment: Alignment.topLeft,
@@ -38,12 +42,91 @@ class _SignuppageState extends State<Signuppage> {
                   ),
                 ),
               ),
-              const Text('Sign up',
-              textAlign: TextAlign.left,
+              SizedBox(
+                height: 31.47,
               ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: const Text(
+                  'List',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.00,
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'Save delicious recipes and get your personal content',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 54.0,
+              ),
+              Formbox(hintText: 'Name'),
+              SizedBox(
+                height: 18,
+              ),
+              Formbox(hintText: 'Email'),
+               SizedBox(
+                height: 18,
+              ),
+              Formbox(hintText: 'Password'),
+               SizedBox(
+                height: 20,
+              ),
+              Formbox(hintText: 'Confirm Password')
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class Formbox extends StatelessWidget {
+  Formbox({@required this.hintText});
+
+  String? hintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50.0,
+      width: 310.0,
+      child: Material(
+       elevation:20,
+        shadowColor: Colors.white60,
+       borderRadius: BorderRadius.circular(16.0),
+        child: TextField(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Color(0xff423B34),
+            hintText: hintText,
+            hintStyle: TextStyle(
+              color: Colors.grey,
+              fontWeight: FontWeight.w400,
+              fontSize: 16,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(15.0),
+              
+              ),
+              borderSide: BorderSide.none,
+            ),
+          ),
+        ),
+
       ),
     );
   }
