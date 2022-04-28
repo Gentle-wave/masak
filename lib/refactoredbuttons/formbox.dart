2601cc11739c3ae60e67ca0ee1e2cac8413723b0
Gentle_wane <adebayooluwatibi2018@gmail.com>
2022-04-28 15:19:23 +0100

    i

---
+++ lib/refactoredbuttons/formbox.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Formbox extends StatelessWidget {
+  Formbox({required this.hintText});
+
+  String hintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50.0,
+      width: 310.0,
+      child: Material(
+       elevation:20,
+        shadowColor: Colors.white60,
+       borderRadius: BorderRadius.circular(16.0),
+        child: TextField(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Color(0xff423B34),
+            hintText: hintText,
+            hintStyle: TextStyle(
+              color: Colors.grey,
+              fontWeight: FontWeight.w400,
+              fontSize: 16,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(15.0),
+              
+              ),
+              borderSide: BorderSide.none,
+            ),
+          ),
+        ),
+
+      ),
+    );
+  }
+}
